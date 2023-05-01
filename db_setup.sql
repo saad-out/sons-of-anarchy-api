b9bd3001e0fed61d3c04a3942fd447817a22f4cd
saad-out <outsaad03@gmail.com>
2023-05-01 02:49:34 +0100

    clenup db setup files

--- db_setup.sql
+++
@@ -1,7 +0,0 @@
--- Create database & user for the SOA API project
-DROP DATABASE IF EXISTS `SOA`;
-CREATE DATABASE IF NOT EXISTS `SOA`;
-CREATE USER IF NOT EXISTS 'soa'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON `SOA`.* TO 'soa'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'soa'@'localhost';
-FLUSH PRIVILEGES;
