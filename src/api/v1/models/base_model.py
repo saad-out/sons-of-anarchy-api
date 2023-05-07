b0839f9818cb2b70018916db5f793cfa17b15241
saad-out <outsaad03@gmail.com>
2023-05-07 01:53:12 +0100

    type annotations

--- src/api/v1/models/base_model.py
+++ src/api/v1/models/base_model.py
@@ -1,15 +1,17 @@
 from api.v1.utils.database import db
 
+from sqlalchemy.orm import Mapped
+from datetime import datetime
 import uuid
 
 class BaseModel(db.Model):
     __abstract__ = True
 
-    recordId = db.Column(db.String(36), primary_key=True)
-    createdAt = db.Column(db.DateTime, default=db.func.now(), nullable=False)
-    updatedAt = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    recordId: Mapped[str] = db.Column(db.String(36), primary_key=True)
+    createdAt: Mapped[datetime] = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    updatedAt: Mapped[datetime] = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: tuple, **kwargs: dict):
         self.recordId = str(uuid.uuid4())
         if kwargs:
             for key, value in kwargs.items():
--- src/api/v1/models/episode.py
+++ src/api/v1/models/episode.py
@@ -1,16 +1,19 @@
 from api.v1.utils.database import db
 from api.v1.models.base_model import BaseModel
 
+from sqlalchemy.orm import Mapped
+from datetime import datetime
+
 
 class Episode(BaseModel, db.Model):
     __tablename__ = 'episodes'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    seasonNumber = db.Column(db.String(64), db.ForeignKey('seasons.seasonOrder'), nullable=False)
-    episodeNumber = db.Column(db.Integer, nullable=False)
-    title = db.Column(db.String(64), nullable=False)
-    synopsis = db.Column(db.Text, nullable=False)
-    airDate = db.Column(db.DateTime, nullable=False)
+    id: Mapped[int] = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    seasonNumber: Mapped[str] = db.Column(db.String(64), db.ForeignKey('seasons.seasonOrder'), nullable=False)
+    episodeNumber: Mapped[int] = db.Column(db.Integer, nullable=False)
+    title: Mapped[str] = db.Column(db.String(64), nullable=False)
+    synopsis: Mapped[str] = db.Column(db.Text, nullable=False)
+    airDate: Mapped[datetime] = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return f'Episode {self.episodeNumber} from season {self.seasonNumber}'
