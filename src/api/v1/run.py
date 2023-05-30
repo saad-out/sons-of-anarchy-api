6ea4b92001cda8ac727368a692257ea0c80eec74
saad-out <outsaad03@gmail.com>
2023-05-30 16:47:53 +0100

    host, port & debug as env variables

--- src/api/v1/run.py
+++ src/api/v1/run.py
@@ -6,11 +6,15 @@ from flask import (
 )
 from flask_migrate import Migrate
 from flask_cors import CORS
+from dotenv import load_dotenv
+from os import getenv
 
 from api.v1.models import *
 from api.v1.routes import app_routes
 from api.v1.utils.database import db
 from api.v1.utils.config import Config
+
+load_dotenv()
 
 app: Flask = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -22,40 +26,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_routes)
 
 
-@app.route('/')
-def index() -> str:
-    return 'Hello, World! and Welcome to the Sons Of Anarchy API version 1 !'
-
-@app.route('/characters')
-def characters() -> str:
-    chars: list[Character] = db.session.query(Character).all()
-    if chars:
-        return chars[0].fullName
-    else:
-        return 'No characters found.'
-    
-@app.route('/seasons')
-def seasons() -> str:
-    seasons: list[Season] = db.session.query(Season).all()
-    if seasons:
-        return seasons[0].title
-    else:
-        return 'No seasons found.'
-    
-
-@app.route('/episodes')
-def episodes() -> str:
-    episodes: list[Episode] = db.session.query(Episode).all()
-    if episodes:
-        return episodes[0].title
-    else:
-        return 'No episodes found.'
-
-
 @app.errorhandler(404)
 def not_found(error) -> Response:
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=getenv('APP_HOST', 'localhost'),
+            port=int(getenv('APP_PORT', 5000)),
+            debug=bool(getenv('DEBUG', False)))
