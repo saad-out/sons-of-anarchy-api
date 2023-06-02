d44f739f909221c25a6d23371266cb605b18331d
saad-out <outsaad03@gmail.com>
2023-06-02 11:26:44 +0100

    document run.py entry file

--- src/api/v1/run.py
+++ src/api/v1/run.py
@@ -1,3 +1,16 @@
+"""
+run.py: Flask API for the Sons of Anarchy TV show.
+
+This module sets up a Flask application for a RESTful API related to the Sons of Anarchy TV show.
+It initializes the necessary components, such as the database, routes, and error handlers.
+
+The API provides endpoints for accessing and manipulating data related to the Sons of Anarchy TV show.
+
+Usage:
+    - Make sure the required dependencies are installed (Flask, Flask-Migrate, Flask-Cors, dotenv...).
+    - Set the necessary environment variables in a .env file.
+    - Run this module to start the Flask application.
+"""
 from flask import (
     Flask,
     jsonify,
@@ -14,24 +27,45 @@ from api.v1.routes import app_routes
 from api.v1.utils.database import db
 from api.v1.utils.config import Config
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Initialize Flask application
 app: Flask = Flask(__name__)
+
+# Configure Flask application
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
+
+# Database and migrations
 db.init_app(app)
 migrate: Migrate = Migrate(app, db)
+
+# Enable CORS
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Register routes
 app.register_blueprint(app_routes)
 
 
 @app.errorhandler(404)
 def not_found(error) -> Response:
+    """
+    Error handler for 404 Not Found errors.
+    
+    Args:
+        error: The error that occurred.
+    
+    Returns:
+        A Flask response containing a JSON object with an error message.
+    """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
+    """
+    Run the Flask application.
+    """
     app.run(host=getenv('APP_HOST', 'localhost'),
             port=int(getenv('APP_PORT', 5000)),
             debug=bool(getenv('DEBUG', False)))
