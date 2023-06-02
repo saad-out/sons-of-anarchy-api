8aa3eaab3b53199fce059ed66cd2d1a8604a32f2
saad-out <outsaad03@gmail.com>
2023-06-02 17:58:01 +0100

    exclude /search endpoint

--- src/api/v1/routes/__init__.py
+++ src/api/v1/routes/__init__.py
@@ -26,6 +26,6 @@ app_routes: Blueprint = Blueprint('app_routes', __name__, url_prefix='/api/v1')
 from api.v1.routes.characters import *
 from api.v1.routes.seasons import *
 from api.v1.routes.episodes import *
-from api.v1.routes.search import *
+# from api.v1.routes.search import *
 from api.v1.routes.auth import *
 from api.v1.routes.images import *
